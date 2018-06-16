acc2254c987eda9fbc2c559142a33d7bcb63da82
Álex García <alxgcrz@outlook.com>
2018-06-16 23:49:18 +0200

    Updated prototype pattern

--- src/creational/prototype/Prototype.java
+++ src/creational/prototype/Prototype.java
@@ -1,16 +1,19 @@
 /*
- * Copyright (c) 2018 @alxgcrz <alxgcrz@outlook.com>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * ************************************************************************
+ *  Copyright (c) 2018 @alxgcrz <alxgcrz@outlook.com>
  *
- *        https://www.apache.org/licenses/LICENSE-2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ***********************************************************************
  */
 
 package creational.prototype;
@@ -18,37 +21,12 @@ package creational.prototype;
 import creational.prototype.example.FactoriaPrototipo;
 import creational.prototype.example.Producto;
 
-
-/**
- * Project: Learning_Design_Patterns_Java
- * Date: 20/09/15
- *
- * @author "Álex García (@alxgcrz) <alxgcrz@outlook.com"
- *
- *
- * Patrones de CREACIÓN
- * --------------------
- * Corrresponden a patrones de diseño que solucionan problemas de creación de instancias.
- *
- *
- * Pattern [PROTOTYPE] (prototipo)
- *
- * Proporciona abstracción a la hora de crear diferentes objetos en un contexto donde se desconoce cúantos y cuáles
- * deben ser creados a priori. La idea principal es que los objetos deben poder clonarse en tiempo de ejecución.
- * Por tanto, este patrón tiene como finalidad crear nuevos objetos duplicándolos, clonando una instancia creada previamente.
- */
 public class Prototype {
 
-    public static void main( String[] args ) {
-
-        // Instancia de la factoría
+    public static void main(String[] args) {
         FactoriaPrototipo factoriaPrototipo = new FactoriaPrototipo();
-
-        // Producto clonado
         Producto producto = (Producto) factoriaPrototipo.create();
-
-        System.out.println( producto.toString() );
-
+        System.out.println(producto.toString());
     }
 
 }
--- src/creational/prototype/example/ProductoA.java
+++ src/creational/prototype/example/ProductoA.java
@@ -1,39 +1,27 @@
 /*
- * Copyright (c) 2018 @alxgcrz <alxgcrz@outlook.com>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * ************************************************************************
+ *  Copyright (c) 2018 @alxgcrz <alxgcrz@outlook.com>
  *
- *        https://www.apache.org/licenses/LICENSE-2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ***********************************************************************
  */
 
 package creational.prototype.example;
 
-/**
- * Project: Learning_Design_Patterns_Java
- * Date: 20/09/15
- *
- * @author "Álex García (@alxgcrz) <alxgcrz@outlook.com"
- *
- * Prototipo que será clonado
- */
 public class ProductoA implements Producto {
 
-    // Name
     private String name = "ProductoA";
 
-
-    /**
-     * Clone productA
-     *
-     * @return Object
-     */
     @Override
     public Object clone() {
         return new ProductoA();
@@ -44,4 +32,5 @@ public class ProductoA implements Producto {
     public String toString() {
         return name;
     }
+
 }
