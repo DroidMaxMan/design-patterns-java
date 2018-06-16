d26549f8a00014c0a2974588d4364c1e25378e67
Álex García <alxgcrz@outlook.com>
2018-06-16 20:11:56 +0200

    Updated visitor pattern

--- src/behavioral/visitor/Visitor.java
+++ src/behavioral/visitor/Visitor.java
@@ -1,16 +1,19 @@
 /*
- * Copyright (c) 2018 @alxgcrz <alxgcrz@outlook.com>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * ************************************************************************
+ *  Copyright (c) 2018 @alxgcrz <alxgcrz@outlook.com>
  *
- *        https://www.apache.org/licenses/LICENSE-2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ***********************************************************************
  */
 
 package behavioral.visitor;
@@ -20,28 +23,9 @@ import behavioral.visitor.example.ConcreteVisitor2;
 import behavioral.visitor.example.ElementA;
 import behavioral.visitor.example.ElementB;
 
-
-/**
- * Project: Learning_Design_Patterns_Java
- * Date: 28/09/15
- *
- * @author "Álex García (@alxgcrz) <alxgcrz@outlook.com"
- *
- *
- * Patrones de COMPORTAMIENTO
- * --------------------
- * Son los patrones de diseño software que se centran en cómo diseñar los sistemas para obtener cierta funcioanlidad y,
- * al mismo tiempo, un diseño escalable.
- *
- *
- * Pattern [Visitor] (Visitante)
- *
- * Permite definir nuevas operaciones sobre una jerarquía de clases sin modificar las clases sobre las que opera.
- */
 public class Visitor {
 
-    public static void main( String[] args ) {
-
+    public static void main(String[] args) {
         // Visitantes
         ConcreteVisitor1 concreteVisitor1 = new ConcreteVisitor1();
         ConcreteVisitor2 concreteVisitor2 = new ConcreteVisitor2();
@@ -51,8 +35,8 @@ public class Visitor {
         ElementB elementB = new ElementB();
 
         // Visitar elementos
-        concreteVisitor1.visitElementA( elementA );
-        concreteVisitor2.visitElementB( elementB );
-
+        concreteVisitor1.visitElementA(elementA);
+        concreteVisitor2.visitElementB(elementB);
     }
+
 }
