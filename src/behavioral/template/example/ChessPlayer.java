337986cfa0c8a86dbd752eaa79aca9cede70b1dc
Álex García <alxgcrz@outlook.com>
2018-06-16 19:49:09 +0200

    Updated template method pattern

---
+++ src/behavioral/template/example/ChessPlayer.java
@@ -0,0 +1,48 @@
+/*
+ * ************************************************************************
+ *  Copyright (c) 2018 @alxgcrz <alxgcrz@outlook.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ***********************************************************************
+ */
+
+package behavioral.template.example;
+
+public class ChessPlayer extends GamePlayer {
+
+    private int movements = 0;
+
+    @Override
+    boolean moveFirst() {
+        System.out.println("El rival mueve primero");
+        return false;
+    }
+
+    @Override
+    void doBestMove() {
+        System.out.println("Moviendo ficha - Movimiento " + movements);
+        movements++;
+    }
+
+    // Como convención para el ejemplo, la partida acaba al alcanzar 50 movimientos
+    @Override
+    boolean isOver() {
+        if (movements < 50) {
+            return false;
+        } else {
+            System.out.println("Fin de la partida - Alcanzado los " + movements + " como máximo");
+            return true;
+        }
+    }
+
+}
