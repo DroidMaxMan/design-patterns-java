0b7385ea2106187e224090a6458454082aad2157
Álex García <alxgcrz@outlook.com>
2018-06-17 00:13:55 +0200

    Updated singleton pattern

--- src/creational/singleton/Singleton.java
+++ src/creational/singleton/Singleton.java
@@ -1,58 +1,44 @@
 /*
- * Copyright (c) 2018 @alxgcrz <alxgcrz@outlook.com>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * ************************************************************************
+ *  Copyright (c) 2018 @alxgcrz <alxgcrz@outlook.com>
  *
- *        https://www.apache.org/licenses/LICENSE-2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ***********************************************************************
  */
+
 package creational.singleton;
 
-/**
- * Project Learning_Design_Patterns_Java
- *
- * @author "Álex García (@alxgcrz) <alxgcrz@outlook.com"
- * Date 19/09/15
- *
- *
- * Patrones de CREACIÓN
- * --------------------
- * Corrresponden a patrones de diseño que solucionan problemas de creación de instancias.
- *
- *
- * Pattern [SINGLETON] (instancia única)
- *
- * Garantiza la existencia de una única instancia para una clase y la creación de un mecanismo de acceso global
- * a dicha instancia. Restringe la instanciación de una clase o valor de un tipo a un solo objeto.
- */
 public class Singleton {
 
     private static Singleton INSTANCE = null;
 
-
     // El constructor privado no permite que se genere un constructor por defecto.
-    // (con mismo modificador de acceso que la definición de la clase)
     private Singleton() {
     }
 
-
-    public Singleton getINSTANCE() {
-        if ( INSTANCE == null ) {
+    public static Singleton getINSTANCE() {
+        if (INSTANCE == null) {
             INSTANCE = new Singleton();
         }
         return INSTANCE;
     }
 
-
-    //Así se podría clonar el objeto y no tendría unicidad.
-    //Singleton clonedObject = (Singleton) new Singleton().clone();
-
+    public static void main(String[] args) {
+        Singleton singleton = getINSTANCE();
+        System.out.println(singleton.toString());
+        singleton = getINSTANCE();
+        System.out.println(singleton.toString());
+    }
 
     //El método "clone" es sobreescrito por el siguiente que arroja una excepción:
     public Object clone() throws CloneNotSupportedException {
--- src/creational/singleton/SingletonSyncronized.java
+++ src/creational/singleton/SingletonSyncronized.java
@@ -1,55 +1,45 @@
 /*
- * Copyright (c) 2018 @alxgcrz <alxgcrz@outlook.com>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * ************************************************************************
+ *  Copyright (c) 2018 @alxgcrz <alxgcrz@outlook.com>
  *
- *        https://www.apache.org/licenses/LICENSE-2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ***********************************************************************
  */
+
 package creational.singleton;
 
-/**
- * Project Learning_Design_Patterns_Java
- *
- * @author "Álex García (@alxgcrz) <alxgcrz@outlook.com"
- * Date 19/09/15
- * @see Singleton
- */
 public class SingletonSyncronized {
 
     private static SingletonSyncronized INSTANCE = null;
 
-
     // El constructor privado no permite que se genere un constructor por defecto.
-    // (con mismo modificador de acceso que la definición de la clase)
     private SingletonSyncronized() {
     }
 
-
     // creador sincronizado para protegerse de posibles problemas  multi-hilo
     // otra prueba para evitar instanciación múltiple
-    private synchronized static void createInstance() {
+    private static synchronized void createInstance() {
         if ( INSTANCE == null ) {
             INSTANCE = new SingletonSyncronized();
         }
     }
 
-
     public static SingletonSyncronized getInstance() {
-        if ( INSTANCE == null ) createInstance();
+        if (INSTANCE == null) {
+            createInstance();
+        }
         return INSTANCE;
     }
-
-
-    //Así se podría clonar el objeto y no tendría unicidad.
-    //Singleton clonedObject = (Singleton) new Singleton().clone();
-
 
     //El método "clone" es sobreescrito por el siguiente que arroja una excepción:
     public Object clone() throws CloneNotSupportedException {
